a8aee48b69267ac75b8d38faedb834d1b3c4e163
EvanWagg <200340662@student.georgianc.on.ca>
2017-05-19 10:38:18 -0400

    added some stuff

--- saveAlbum.php
+++ saveAlbum.php
@@ -2,9 +2,33 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>$Title$</title>
+	<title>Saving Album...</title>
 </head>
 <body>
-$END$
+	<?php
+		$title = $_POST['title'];
+		$year = $_POST['year'];
+		$artist = $_POST['artist'];
+
+		echo 'title: '.$title.'<br />';
+		echo 'year: '.$year.'<br />';
+		echo 'artist: '.$artist.'<br />';
+
+		//Step 1 -  conenct to the db
+		$conn = new PDO('mysql:host=aws.computerstudi.es;dbname=gc200340662', 'gc200340662','uozYSDupBu');
+		//Step 2 - create the SQL command and INSERT a record
+		$sql = "INSERT INTO albums (title, year, artist) 
+							VALUES (:title, :year, :artist);";
+		//Step 3 - prepare SQL command and prevent SQL injection
+		$cmd = $conn->prepare($sql);
+		$cmd->bindParam('title', $title, PDO::PARAM_STR, 50);
+		$cmd->bindParam('year', $year, PDO::PARAM_INT, 4);
+		$cmd->bindParam('artist', $artist, PDO::PARAM_STR, 50);
+		//Step 4 - execute
+		$cmd->execute();
+		//Step 5 - disconnect from db
+		$conn = null;
+		//Step 6 - redirect to albums page
+	?>
 </body>
 </html>
